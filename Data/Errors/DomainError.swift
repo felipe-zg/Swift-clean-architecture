4e174b2b006a774a4d4d4b918f467d5cb75c75bd
felipe <felipe@felipe.com>
2020-11-22 18:12:10 -0300

    test: ensure RemoteAddAccount completes with error if HttpPostClient completes with error

---
+++ Data/Errors/DomainError.swift
@@ -0,0 +1,8 @@
+//
+//  DomainError.swift
+//  Data
+//
+//  Created by Felipe Zêba on 22/11/20.
+//
+
+import Foundation
---
+++ Data/Http/Errors/HttpError.swift
@@ -0,0 +1,8 @@
+//
+//  HttpError.swift
+//  Data
+//
+//  Created by Felipe Zêba on 22/11/20.
+//
+
+import Foundation
