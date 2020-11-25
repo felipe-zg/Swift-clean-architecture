5586cc2b93bb2b0e4a882afc3aa00c978fdb43de
felipe <felipe@felipe.com>
2020-11-25 14:45:54 -0300

    feat: add Infra layer

---
+++ Infra/Infra.h
@@ -0,0 +1,18 @@
+//
+//  Infra.h
+//  Infra
+//
+//  Created by Felipe Zêba on 25/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Infra.
+FOUNDATION_EXPORT double InfraVersionNumber;
+
+//! Project version string for Infra.
+FOUNDATION_EXPORT const unsigned char InfraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Infra/PublicHeader.h>
+
+
