b73fc0cbee342ecf5b62a503848d006a7f475638
felipe <felipe@felipe.com>
2020-11-27 09:58:36 -0300

    test: add randomization and sanitizer to infra and Data layers

--- DataTests/Helpers/HttpPostClientSpy.swift
+++ DataTests/Helpers/HttpPostClientSpy.swift
@@ -4,7 +4,7 @@ import Data
 class HttpPostClientSpy: HttpPostClient{
     var urls = [URL]()
     var data: Data?
-    var completion: ((Result<Data, HttpError>) -> Void)?
+    var completion: ((Result<Data?, HttpError>) -> Void)?
     
     func post(to url: URL, with data: Data?, completion: @escaping (Result<Data?, HttpError>) -> Void) {
         self.urls.append(url)
