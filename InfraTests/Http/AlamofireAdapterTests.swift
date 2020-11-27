74d9f32a9f2452af76ed09eb7af91df99a151ebb
felipe <felipe@felipe.com>
2020-11-27 09:51:18 -0300

    refactor: move UrlProtocolStub to its own file

--- InfraTests/Http/AlamofireAdapterTests.swift
+++ InfraTests/Http/AlamofireAdapterTests.swift
@@ -91,45 +91,4 @@ extension AlamofireAdapterTests {
     }
 }
 
-class UrlProtocolStub: URLProtocol {
-    static var emit: ((URLRequest) -> Void)?
-    static var data: Data?
-    static var response: HTTPURLResponse?
-    static var error: Error?
-    
-    static func observeRequest(completion: @escaping (URLRequest) -> Void){
-        self.emit = completion
-    }
-    
-    override class func canInit(with request: URLRequest) -> Bool {
-        return true
-    }
-    
-    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-        return request
-    }
-    
-    static func simulate(data: Data?, response: HTTPURLResponse?, error: Error?){
-        UrlProtocolStub.data = data
-        UrlProtocolStub.response = response
-        UrlProtocolStub.error = error
-    }
-    
-    override func startLoading() {
-        UrlProtocolStub.emit?(request)
-        if let data = UrlProtocolStub.data {
-            client?.urlProtocol(self, didLoad: data)
-        }
-        if let response = UrlProtocolStub.response {
-            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
-        }
-        if let error = UrlProtocolStub.error {
-            client?.urlProtocol(self, didFailWithError: error)
-        }
-        client?.urlProtocolDidFinishLoading(self)
-    }
-    
-    override func stopLoading() {}
-    
-}
 
