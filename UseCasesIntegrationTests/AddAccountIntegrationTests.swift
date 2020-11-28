834632c189113d94483d0ac3a5dff3aff4f5601e
felipe <felipe@felipe.com>
2020-11-28 09:01:12 -0300

    test: add integration tests target

---
+++ UseCasesIntegrationTests/AddAccountIntegrationTests.swift
@@ -0,0 +1,15 @@
+//
+//  UseCasesIntegrationTests.swift
+//  UseCasesIntegrationTests
+//
+//  Created by Felipe Zêba on 28/11/20.
+//
+
+import XCTest
+
+class AddAccountIntegrationTests: XCTestCase {
+
+    func testExample() throws {
+        
+    }
+}
