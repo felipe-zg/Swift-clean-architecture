2e65217fd6aa2268237a0a5f52a0261b26a71009
felipe <felipe@felipe.com>
2020-11-28 09:29:26 -0300

    test: ensure add account integration with api return an account model on success

--- UseCasesIntegrationTests/AddAccountIntegrationTests.swift
+++ UseCasesIntegrationTests/AddAccountIntegrationTests.swift
@@ -1,15 +1,26 @@
-//
-//  UseCasesIntegrationTests.swift
-//  UseCasesIntegrationTests
-//
-//  Created by Felipe Zêba on 28/11/20.
-//
-
 import XCTest
+import Data
+import Infra
+import Domain
 
 class AddAccountIntegrationTests: XCTestCase {
 
-    func testExample() throws {
-        
+    func test_add_account() throws {
+        let alamofireAdapter = AlamofireAdapter()
+        let apiUrl = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
+        let sut = RemoteAddAccount(url: apiUrl, httpPostClient: alamofireAdapter)
+        let addAccountModel = AddAccountModel(name: "felipe zeba", email: "felipe@gmail.com", password: "my_password", passwordConfirmation: "my_password")
+        let exp = expectation(description: "waiting integration test")
+        sut.add(addAccountModel: addAccountModel) { (result) in
+            switch result {
+            case .failure: XCTFail("expected success but got \(result) instead")
+            case .success(let account):
+                XCTAssertNotNil(account.id)
+                XCTAssertEqual(account.name, addAccountModel.name)
+                XCTAssertEqual(account.email, addAccountModel.email)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 15)
     }
 }
