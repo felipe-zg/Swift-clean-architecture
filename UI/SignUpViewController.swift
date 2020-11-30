cf22aa573b7424821b26d6bc317f89c27f0ba9c3
felipe <felipe@felipe.com>
2020-11-30 11:57:06 -0300

    test: ensure lSignUpViewController has a loading indicator and it starts hidden by default

---
+++ UI/SignUpViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
