3cefcadfe420c995a21a84b01d6379c468ec8369
felipe <felipe@felipe.com>
2020-11-30 09:14:10 -0300

    refactor: move SignUpViewModel to a separate file

--- Presentation/Presenters/SignUp/SignUpPresenter.swift
+++ Presentation/Presenters/SignUp/SignUpPresenter.swift
@@ -55,17 +55,3 @@ public final class SignUpPresenter {
         return AlertViewModel(title: "Invalid field", message: "\(field) is invalid")
     }
 }
-
-public struct SignUpViewModel {
-    public var name: String?
-    public var email: String?
-    public var password: String?
-    public var passwordConfirmation: String?
-    
-    public init(name: String? = nil, email: String? = nil, password: String? = nil, passwordConfirmation: String? = nil) {
-        self.name = name
-        self.email = email
-        self.password = password
-        self.passwordConfirmation = passwordConfirmation
-    }
-}
