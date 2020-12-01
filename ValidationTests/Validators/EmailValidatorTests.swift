4e57c9580f3506eb62190af917e4a2b7188bd9f0
felipe <felipe@felipe.com>
2020-12-01 07:46:32 -0300

    test: ensure EmailValidatorAdapter returns true if email is valid

--- ValidationTests/Validators/EmailValidatorTests.swift
+++ ValidationTests/Validators/EmailValidatorTests.swift
@@ -3,13 +3,21 @@ import Validation
 
 class EmailValidatorTests: XCTestCase {
 
-    func test_() throws {
+    func test_EmailValidatorAdapter_should_return_false_if_email_is_invalid() throws {
         let sut = makeSut()
         XCTAssertFalse(sut.isValid(email: "something"))
         XCTAssertFalse(sut.isValid(email: "something@anything"))
         XCTAssertFalse(sut.isValid(email: "@something.com"))
         XCTAssertFalse(sut.isValid(email: "something@anything."))
         XCTAssertFalse(sut.isValid(email: "something@anything.c"))
+    }
+    
+    func test_EmailValidatorAdapter_should_return_true_if_email_is_valid() throws {
+        let sut = makeSut()
+        XCTAssertTrue(sut.isValid(email: "something@yahoo.com.br"))
+        XCTAssertTrue(sut.isValid(email: "something@hotmail.com"))
+        XCTAssertTrue(sut.isValid(email: "something@outlook.ca"))
+        XCTAssertTrue(sut.isValid(email: "something@spark.net"))
     }
 }
 
