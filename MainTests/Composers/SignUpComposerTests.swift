37363b454f19aad223f4725eba4076249fe9d57d
felipe <felipe_zeba@outlook.com>
2020-12-02 14:56:57 -0300

    refactor: change fila name

--- MainTests/Composers/SignUpComposerTests.swift
+++ MainTests/Composers/SignUpComposerTests.swift
@@ -1,9 +1,10 @@
 import XCTest
 import Main
 
-class SignUpIntegrationTests: XCTestCase {
-    func testExample() throws {
+class SignUpComposerTests: XCTestCase {
+    func test_ui_presentation_integration() throws {
         let sut = SignUpComposer.composeControllerWith(addAccount: AddAccountSpy())
         checkMemoryLeak(for: sut)
     }
 }
+
