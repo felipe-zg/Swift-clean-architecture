d32fa601c426644cac681006024a54e07d6b2d2c
felipe <felipe@felipe.com>
2020-12-02 11:09:42 -0300

    delete: remove ControllerFactory and passed the logic to the SignUpComponer

--- Main/Composers/SignUpComposer.swift
+++ Main/Composers/SignUpComposer.swift
@@ -1,9 +1,15 @@
 import Foundation
 import Domain
+import Validation
+import Presentation
 import UI
 
 public final class SignUpComposer {
     public static func composeControllerWith(addAccount: AddAccount) -> SignUpViewController{
-        return ControllerFactory.makeSignUp(addAccount: addAccount)
+        let controller = SignUpViewController.instantiate()
+        let emailValidatorAdapter = EmailValidatorAdapter()
+        let presenter = SignUpPresenter(alertView: WeakVarProxy(controller), loadingView: WeakVarProxy(controller), emailValidator: emailValidatorAdapter, addAccount: addAccount)
+        controller.signUp = presenter.signUp
+        return controller
     }
 }
