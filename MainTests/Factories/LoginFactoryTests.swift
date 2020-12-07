ee7807a15a1548864ad7b1afa6079111280785dc
felipe <felipe_zeba@outlook.com>
2020-12-07 18:47:38 -0300

    test: ensure background tasks complete in the main thread

--- MainTests/Factories/LoginFactoryTests.swift
+++ MainTests/Factories/LoginFactoryTests.swift
@@ -4,6 +4,18 @@ import UI
 import Validation
 
 class LoginFactoryTests: XCTestCase {
+    func test_background_task_should_complete_on_main_thread() throws {
+        let (sut, authenticationSpy) = makeSut()
+        sut.loadViewIfNeeded()
+        sut.login?(makeLoginViewModel())
+        let exp = expectation(description: "eaiting")
+        DispatchQueue.global().async {
+            authenticationSpy.completeWithError(.unexpected)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+    
     func test_login_composes_with_correct_validations() throws {
         let validations = makeLoginValidations()
         XCTAssertEqual(validations[0] as! RequiredFieldValidation, RequiredFieldValidation(fieldName: "email", fieldLabel: "e-mail"))
@@ -15,7 +27,7 @@ class LoginFactoryTests: XCTestCase {
 extension LoginFactoryTests {
     func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (sut: LoginViewController, authenticationSpy: AuthenticationSpy) {
         let authenticationSpy = AuthenticationSpy()
-        let sut = makeLoginController(authentication: authenticationSpy)
+        let sut = makeLoginController(authentication: MainQueueDispatchDecorator(authenticationSpy))
         checkMemoryLeak(for: sut, file: file, line: line)
         checkMemoryLeak(for: authenticationSpy, file: file, line: line)
         return (sut, authenticationSpy)
