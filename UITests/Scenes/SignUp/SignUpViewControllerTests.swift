836f47685f4bba3dd1027f68eded2650389eba79
felipe <felipe_zeba@outlook.com>
2020-12-07 18:01:48 -0300

    refactor: simplified test

--- UITests/Scenes/SignUp/SignUpViewControllerTests.swift
+++ UITests/Scenes/SignUp/SignUpViewControllerTests.swift
@@ -5,21 +5,16 @@ import Presentation
 
 class SignUpViewControllerTests: XCTestCase {
     func test_loading_is_hidden_on_start() throws {
-        let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
-        let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
-        sut.loadViewIfNeeded()
-        XCTAssertEqual(sut.loadingIndicator?.isAnimating, false)
+        XCTAssertEqual(makeSut().loadingIndicator?.isAnimating, false)
     }
     
     func test_it_implements_LoadingView() throws {
         XCTAssertNotNil(makeSut() as LoadingView)
     }
     
-    
     func test_it_implements_AlertView() throws {
         XCTAssertNotNil(makeSut() as AlertView)
     }
-    
     
     func test_saveButton_calls_signUp_on_tap() throws {
         var signUpViewModel: SignUpViewModel?
