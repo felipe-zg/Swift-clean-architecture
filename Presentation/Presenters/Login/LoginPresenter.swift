76d2ea10b87595cf34f6daf680ad3308615541f2
felipe <felipe_zeba@outlook.com>
2020-12-07 16:19:53 -0300

    feat: ensure LoginPresenter.auth calls authentication with correct values

--- Presentation/Presenters/Login/LoginPresenter.swift
+++ Presentation/Presenters/Login/LoginPresenter.swift
@@ -4,15 +4,21 @@ import Domain
 public final class LoginPresenter {
     private let alertiView: AlertView
     private let validation: Validation
+    private let authentication: Authentication
     
-    public init(alertiView: AlertView, validation: Validation) {
+    public init(alertiView: AlertView, validation: Validation, authentication: Authentication) {
         self.validation = validation
         self.alertiView = alertiView
+        self.authentication = authentication
     }
     
     public func login(viewModel: LoginViewModel) {
         if let message = validation.validate(data: viewModel.toJSON()) {
             alertiView.showMessage(AlertViewModel(title: "Validation failed", message: message))
+        } else {
+            authentication.auth(authenticationModel: viewModel.toAuthenticationAccountModel()) { [weak self] (result) in
+                guard let self = self else { return }
+            }
         }
     }
 }
